0ad9cb3b28a6b1963fbddcad6a7d24adcc40bd64
Mustafa Haita <mustafahaitaa@gmail.com>
2024-08-14 19:15:22 +0300

    Challenge Finish Screen

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { useState, useTransition } from "react";
+import { useAudio, useWindowSize } from "react-use";
+import Image from "next/image";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import Confetti from "react-confetti";
 
 import { challengeOptions, challenges } from "@/db/schema";
 import Header from "./header";
@@ -8,8 +13,8 @@ import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
 import { Footer } from "./footer";
 import { upsertChallengeProgres } from "@/actions/challenge-progress";
-import { toast } from "sonner";
 import { reduceHearts } from "@/actions/user-progress";
+import { ResultCard } from "./result-card";
 
 type Props = {
     initialPercentage: number;
@@ -29,8 +34,19 @@ const Quiz = ({
     initialLessonChallenges,
     userSubscription,
 }: Props) => {
+    const { width, height } = useWindowSize();
+    const router = useRouter();
+
+    const [finishAudio] = useAudio({ src: "/finish.mp3", autoPlay: true });
+    const [correctAudio, _c, correctControls] = useAudio({
+        src: "/correct.wav",
+    });
+    const [incorrectAudio, _i, incorrectControls] = useAudio({
+        src: "/incorrect.wav",
+    });
     const [pending, startTransition] = useTransition();
 
+    const [lessonId, setLessonId] = useState(initialLessonId);
     const [hearts, setHearts] = useState(initialHearts);
     const [percentage, setPercentage] = useState(initialPercentage);
     const [challenges, setChallenges] = useState(initialLessonChallenges);
@@ -88,6 +104,7 @@ const Quiz = ({
                             return;
                         }
 
+                        correctControls.play();
                         setStatus("correct");
                         setPercentage((prev) => prev + 100 / challenges.length);
 
@@ -108,6 +125,7 @@ const Quiz = ({
                             return;
                         }
 
+                        incorrectControls.play();
                         setStatus("wrong");
 
                         if (!response?.error) {
@@ -121,6 +139,52 @@ const Quiz = ({
         }
     };
 
+    if (!challenge) {
+        return (
+            <>
+                {finishAudio}
+                <Confetti
+                    width={width}
+                    height={height}
+                    recycle={false}
+                    numberOfPieces={500}
+                    tweenDuration={10000}
+                />
+                <div className="flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+                    <Image
+                        src={"/finish.svg"}
+                        alt="Finish"
+                        className="hidden lg:block"
+                        height={100}
+                        width={100}
+                    />
+                    <Image
+                        src={"/finish.svg"}
+                        alt="Finish"
+                        className="block lg:hidden"
+                        height={50}
+                        width={50}
+                    />
+                    <h1 className="text-lg lg:text-3xl text-neutral-700 font-bold">
+                        Great job! <br /> You&apos;ve completed the lesson.{" "}
+                    </h1>
+                    <div className="flex items-center gap-x-4 w-full">
+                        <ResultCard
+                            variant="points"
+                            value={challenges.length * 10}
+                        />
+                        <ResultCard variant="hearts" value={hearts} />
+                    </div>
+                </div>
+                <Footer
+                    lessonId={lessonId}
+                    status="completed"
+                    onCheck={() => router.push("/learn")}
+                />
+            </>
+        );
+    }
+
     const title =
         challenge.type === "ASSIST"
             ? "Select the correct meaning"
@@ -128,6 +192,8 @@ const Quiz = ({
 
     return (
         <>
+            {incorrectAudio}
+            {correctAudio}
             <Header
                 hearts={hearts}
                 percentage={percentage}
